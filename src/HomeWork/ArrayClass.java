2acec3d4118f855f0005a83c67e533ae55d2d24d
Назар Садик <Slameryt4@gmail.com>
2019-12-11 20:26:07 +0200

    HomeWork

---
+++ src/HomeWork/ArrayClass.java
@@ -0,0 +1,54 @@
+package HomeWork;
+
+class ArrayClass
+{
+    private String[] arr;
+
+    public ArrayClass(int lenght)
+    {
+        Create(lenght);
+    }
+
+    private String[] Create(int lenght)
+    {
+        return arr = new String[lenght];
+    }
+
+    public void Add(String[] str)
+    {
+        if(str.length <= arr.length)
+            arr = str;
+    }
+
+    public String getElement(int index)
+    {
+        if(index < arr.length && index >= 0)
+        {
+            return arr[index];
+        }
+        else
+        {
+            return "Выход за пределы массива";
+        }
+    }
+
+//    public String[] ConcatWithCheck(ArrayClass obj)
+//    {
+//        return arr.Contact(obj.arr).Distinct().ToArray();
+//    }
+
+    public void Show(int index)
+    {
+        System.out.println("Элемент = " + arr[index]);
+    }
+
+    public void ShowArray()
+    {
+        String result = null;
+        for (String s : arr)
+        {
+            result += s + ", ";
+        }
+        System.out.println(result);
+    }
+}
